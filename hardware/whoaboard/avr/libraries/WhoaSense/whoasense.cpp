2305c5244509291caaea5a820b9ab41e3d4d3eec
Josh Vekhter <josh@foolishproducts.com>
2017-03-10 03:05:07 -0500

    welllp, fix that silly mistake

--- hardware/whoaboard/avr/libraries/WhoaSense/whoasense.cpp
+++ hardware/whoaboard/avr/libraries/WhoaSense/whoasense.cpp
@@ -12,7 +12,7 @@
 #define hvClock 18     // pf7 -> A0   D18
 
 #define elEnable 20     // pf5 -> A2   D20
-#define elPowerEnable 20// pb7 ->      D11
+#define elPowerEnable 11// pb7 ->      D11
 
 #define clearPin 6     // pd7 -> A7   D6#
 #define sense1 4       // pd4 -> A6   D4
